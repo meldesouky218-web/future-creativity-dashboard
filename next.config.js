f6ea700e2aa9e291e4c21573b80dc7c93adf0723
Mohammed Eldesouky <meldesouky218@gmail.com>
2025-10-29 09:15:10 +0300

    fix: disable ESLint check during Vercel build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // 🚀 هذا السطر يجعل Vercel يتجاهل أخطاء ESLint أثناء عملية الـ build
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
+  images: {
+    domains: ["future-creativity-server.vercel.app"], // لو بتعرض صور من API
+  },
 };
 
 module.exports = nextConfig;
