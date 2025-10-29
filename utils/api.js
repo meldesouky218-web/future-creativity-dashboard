64de50fc17f62015dd8a132d0bea905cc38a7903
Mohammed Eldesouky <meldesouky218@gmail.com>
2025-10-29 11:02:06 +0300

    fix: correct API env variable name

--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:5000/api";
+const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
 
 const API = axios.create({
   baseURL,
